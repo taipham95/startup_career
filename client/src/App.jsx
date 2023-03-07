87fec44e640038af0053032155f10268d63a5c8b
tranminhwang <j2team.tranminhquang@gmail.com>
2023-03-07 13:15:52 +0700

    remove ts and add tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="w-full h-screen flex flex-col items-center bg-gray-900">
+      <h1 className="text-center text-white text-4xl font-bold mt-12">
+        Hello X-Career
+      </h1>
+    </div>
+  );
+}
+
+export default App;
