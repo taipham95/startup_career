7ddc81481c80b9caa9f3881ed713fc914c19e5f2
KhangDao <101658465+KhangDao@users.noreply.github.com>
2023-03-13 14:48:57 +0700

    Update server.ts
    add 404 202

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,19 +12,20 @@ app.use(bodyParser.json());
 
 // Connect to MongoDB
 const options = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  };
-  
-  mongoose.connect('mongodb://localhost:27017/joblistings', options)
-    .then(() => {
-      console.log('MongoDB connected');
-    })
-    .catch((err) => {
-      console.error(err);
-    });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+};
+
+mongoose.connect('mongodb://localhost:27017/joblistings', options)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 // Create job schema
 const jobSchema = new mongoose.Schema({
   name: String,
@@ -50,6 +51,9 @@ const Applicant = mongoose.model('Applicant', applicantSchema);
 // Routes
 app.get('/jobs', async (req, res) => {
   const jobs = await Job.find();
+  if (jobs.length === 0) {
+    return res.status(404).json({ message: "No jobs found" });
+  }
   res.json(jobs);
 });
 
@@ -57,11 +61,14 @@ app.post('/jobs', async (req, res) => {
   const { name, requirements, description } = req.body;
   const job = new Job({ name, requirements, description });
   await job.save();
-  res.json(job);
+  res.status(202).json(job);
 });
 
 app.get('/applicants', async (req, res) => {
   const applicants = await Applicant.find();
+  if (applicants.length === 0) {
+    return res.status(404).json({ message: "No applicants found" });
+  }
   res.json(applicants);
 });
 
@@ -69,7 +76,7 @@ app.post('/applicants', async (req, res) => {
   const { name, email, phone, dob, resumeLink } = req.body;
   const applicant = new Applicant({ name, email, phone, dob, resumeLink });
   await applicant.save();
-  res.json(applicant);
+  res.status(202).json(applicant);
 });
 
 // Start the server
