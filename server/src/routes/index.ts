084a9cc276f79bb7bf016aec40f632fa757d8437
taipham95 <phamanhtaictn@gmail.com>
2023-03-17 21:24:45 +0700

    update BE

---
+++ server/src/routes/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import jobDetail from "./JobDetail"
+import applyApp from "./Apply"
+
+const router = express.Router();
+
+//Job detail KHANG
+router.use("/jobs", jobDetail);
+
+//Apply TAIPHAM
+router.use("/apply", applyApp);
+
+export default router;
+
+
+// import express from "express";
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   return console.log(`Express is listening at http://localhost:${port}`);
+// });
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,74 +1,98 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import cors from 'cors';
-import mongoose, { ConnectOptions } from "mongoose";
-import Job, {IJob} from './job.model';
-import Applicant, { IApplicant } from './applicant.model';
+import apiRouter from "./routes/index"
+import connectToDB from "./config/db"
 
 const app = express();
-const port = 8000;
+const PORT = 3001;
+
+// Connect mongodb
+connectToDB();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Setup routes
+app.use('/careers', apiRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is listening at PORT ${PORT}`);
+});
+
+
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import cors from 'cors';
+// import mongoose, { ConnectOptions } from "mongoose";
+// import Job, {IJob} from './job.model';
+// import Applicant, { IApplicant } from './applicant.model';
+
+// const app = express();
+// const port = 3000;
 
 // Middleware
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
-// Connect to MongoDB
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-};
+// // Connect to MongoDB
+// const options = {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// };
 
-mongoose.connect('mongodb://localhost:27017/joblistings', options)
-  .then(() => {
-    console.log('MongoDB connected');
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-// Routes
-app.get('/jobs', async (req, res) => {
-  const jobs: IJob[] = await Job.find();
-  if (jobs.length === 0) {
-    return res.status(404).json({ message: "No jobs found" });
-  }
-  res.json(jobs);
-});
+// mongoose.connect('mongodb://localhost:27017/joblistings', options)
+//   .then(() => {
+//     console.log('MongoDB connected');
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//   });
 
-app.post('/jobs', async (req, res) => {
-  const { name, requirements, description } = req.body;
-  const job = new Job({ name, requirements, description });
-  job.save((err, doc) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: "An error occurred while saving the job" });
-    }
-    res.status(202).json(doc);
-  });
-});
+// // Routes
+// app.get('/jobs', async (req, res) => {
+//   const jobs: IJob[] = await Job.find();
+//   if (jobs.length === 0) {
+//     return res.status(404).json({ message: "No jobs found" });
+//   }
+//   res.json(jobs);
+// });
 
-app.get('/applicants', async (req, res) => {
-  const applicants: IApplicant[] = await Applicant.find();
-  if (applicants.length === 0) {
-    return res.status(404).json({ message: "No applicants found" });
-  }
-  res.json(applicants);
-});
+// app.post('/jobs', async (req, res) => {
+//   const { name, requirements, description } = req.body;
+//   const job = new Job({ name, requirements, description });
+//   job.save((err, doc) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).json({ message: "An error occurred while saving the job" });
+//     }
+//     res.status(202).json(doc);
+//   });
+// });
 
-app.post('/applicants', async (req, res) => {
-  const { name, email, phone, dob, resumeLink } = req.body;
-  const applicant = new Applicant({ name, email, phone, dob, resumeLink });
-  applicant.save((err, doc) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: "An error occurred while saving the applicant" });
-    }
-    res.status(202).json(doc);
-  });
-});
+// app.get('/applicants', async (req, res) => {
+//   const applicants: IApplicant[] = await Applicant.find();
+//   if (applicants.length === 0) {
+//     return res.status(404).json({ message: "No applicants found" });
+//   }
+//   res.json(applicants);
+// });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
-});
+// app.post('/applicants', async (req, res) => {
+//   const { name, email, phone, dob, resumeLink } = req.body;
+//   const applicant = new Applicant({ name, email, phone, dob, resumeLink });
+//   applicant.save((err, doc) => {
+//     if (err) {
+//       console.error(err);
+//       return res.status(500).json({ message: "An error occurred while saving the applicant" });
+//     }
+//     res.status(202).json(doc);
+//   });
+// });
+
+// // Start the server
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}.`);
+// });
