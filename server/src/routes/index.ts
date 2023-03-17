98274075a5404526e4f7cb36e9a358960cd5d2ab
taipham95 <phamanhtaictn@gmail.com>
2023-03-17 21:30:34 +0700

    update BE taiP

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -15,7 +15,7 @@ export default router;
 
 // import express from "express";
 // const app = express();
-// const port = 3000;
+const port = 3000;
 
 // app.get("/", (req, res) => {
 //   res.send("Hello World!");
