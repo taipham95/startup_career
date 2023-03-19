ff103f8ca4cc5d91dc3ee627b948c9d672e36a6a
shanele147 <shane.le147@gmail.com>
2023-03-19 21:31:57 +0700

    update profile field

--- client/src/components/HeroBanner/HeroBanner.jsx
+++ client/src/components/HeroBanner/HeroBanner.jsx
@@ -3,7 +3,7 @@ import "./HeroBanner.css";
 
 const HeroBanner = ({ tag, title, text }) => {
   return (
-    <div className="hero-banner bg-[url('https://skymavis.com/_app/immutable/assets/ImageBackgroundJobDetailMobile-2dbabf27.png')] md:bg-[url('https://skymavis.com/_app/immutable/assets/axie-concept-f5e52a79.png')] container max-w-full mx-auto text-center flex flex-col align-middle justify-center px-4 md:px-8 py-[6rem] gap-4 lg:gap-6">
+    <div className="hero-banner bg-[url('https://skymavis.com/_app/immutable/assets/ImageBackgroundJobDetailMobile-2dbabf27.png')] md:bg-[url('https://skymavis.com/_app/immutable/assets/axie-concept-f5e52a79.png')] container max-w-full mx-auto text-center flex flex-col align-middle justify-center px-4 md:px-8 gap-4 lg:gap-6">
       <div>
         <span class="inline-block whitespace-nowrap rounded-[0.35rem] bg-sky-500 px-[0.8rem] py-[0.5rem] text-center align-baseline text-[0.75em] font-bold leading-none text-white">
           Engineer
