fb61509cbb85d839cae32ffa9bc8ce71f2fa1d15
tranminhwang <j2team.tranminhquang@gmail.com>
2023-03-31 20:16:44 +0700

    install package

--- client/src/pages/AdminPage/index.jsx
+++ client/src/pages/AdminPage/index.jsx
@@ -1,20 +1,20 @@
-import EmployeesStatus from "./Overview/EmployeesStatus"
-import JobStatistics from "./Overview/JobStatistics"
-import SearchAndInforAdmin from "./SearchAndInforAdmin"
-import Sidebar from "./Sidebar"
+import EmployeesStatus from "./Overview/EmployeesStatus";
+import JobStatistics from "./Overview/JobStatistics";
+import SearchAndInforAdmin from "./SearchAndInforAdmin";
+import Sidebar from "./Sidebar";
 
-const AdminPage =()=>{
-    return(
-        <main class="flex justify-center ">
-        <div class=" flex justify-start border-[1.5px] border-slate-300 ">
-<Sidebar/>
-<div class="flex flex-col">
-<SearchAndInforAdmin/>
-<JobStatistics/>
-<EmployeesStatus/>
-</div>
-        </div></main>
-    )
-    
-}
-export default AdminPage
+const AdminPage = () => {
+  return (
+    <main class="flex justify-center ">
+      <div class=" flex justify-start border-[1.5px] border-slate-300 ">
+        <Sidebar />
+        <div class="flex flex-col">
+          <SearchAndInforAdmin />
+          <JobStatistics />
+          <EmployeesStatus />
+        </div>
+      </div>
+    </main>
+  );
+};
+export default AdminPage;
