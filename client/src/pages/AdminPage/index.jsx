a01ce4aeb805ba67735030a134b3ce43c92cc9fb
TonyTaii <pinpok65@gmail.com>
2023-03-31 13:30:40 +0700

    Add Admin Page

--- client/src/pages/AdminPage/index.jsx
+++ client/src/pages/AdminPage/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import SearchAndInforAdmin from "./SearchAndInforAdmin";
 import Sidebar from "./Sidebar";
 import Overview from "./Overview/Overview";
@@ -30,3 +31,25 @@ const AdminPage = () => {
   );
 };
 export default AdminPage;
+=======
+import EmployeesStatus from "./Overview/EmployeesStatus"
+import JobStatistics from "./Overview/JobStatistics"
+import SearchAndInforAdmin from "./SearchAndInforAdmin"
+import Sidebar from "./Sidebar"
+
+const AdminPage =()=>{
+    return(
+        <main class="flex justify-center ">
+        <div class=" flex justify-start border-[1.5px] border-slate-300 ">
+<Sidebar/>
+<div class="flex flex-col">
+<SearchAndInforAdmin/>
+<JobStatistics/>
+<EmployeesStatus/>
+</div>
+        </div></main>
+    )
+    
+}
+export default AdminPage
+>>>>>>> 7c41994 (Add Admin Page)
