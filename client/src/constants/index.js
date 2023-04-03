025f1f42499993ccb355a9302075126c1d785ea6
shanele147 <shane.le147@gmail.com>
2023-04-03 15:13:47 +0700

    test POST Apply

---
+++ client/src/constants/index.js
@@ -0,0 +1,5 @@
+export const SERVER_PORT = 8001;
+export const BASE_URL = `http://localhost:${SERVER_PORT}/careers`;
+
+export const JOBS_ENDPOINT = "/jobs";
+export const APPLY_ENDPOINT = "/apply";
