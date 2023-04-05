50c72a71914a536a027e95ae86fb77770a1fb812
TonyTaii <pinpok65@gmail.com>
2023-04-05 18:21:56 +0700

    update admin page v2

--- client/src/pages/AdminPage/index.jsx
+++ client/src/pages/AdminPage/index.jsx
@@ -5,15 +5,15 @@ import Sidebar from "./Sidebar";
 
 const AdminPage = () => {
   return (
-    <main class="flex justify-center w-[100vw]">
-      <div class=" flex justify-start border-[1.5px] border-slate-300 w-full">
-        <Sidebar class="w=[1%]"/>
-        <div class="flex flex-col w-[99%]">
+    <main class="flex justify-center w-[100%]">
+      
+        <Sidebar class="w=[25%]"/>
+        <div class="flex flex-col w-[100%]">
           <SearchAndInforAdmin />
           <JobStatistics />
           <EmployeesStatus />
         </div>
-      </div>
+    
     </main>
   );
 };
