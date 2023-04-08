eb6a626aced90ca14b401ea6056cf45010c67606
taipham95 <phamanhtaictn@gmail.com>
2023-04-08 14:06:45 +0700

    Update Authen-login-register, middlewares apply,job

--- server/src/routes/userRouter.ts
+++ server/src/routes/userRouter.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import UserAdmin from './users';
 import Login from './login'
+import Register from './register'
 
 const router = express.Router();
 
@@ -9,5 +10,8 @@ router.use('/users', UserAdmin)
 
 // API login, authenticator ... http://localhost:8001/admin/login
 router.use('/login', Login);
+
+//API Register ... http://localhost:8001/admin/register
+router.use('/register', Register);
 
 export default router;
--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -3,19 +3,20 @@ import userAdminController from '../controllers/userAdminController';
 import authMdw from '../middlewares/authMdw';
 const router = express.Router();
 
+
+//MiddleWares
+router.use(authMdw.authMdw, authMdw.adminMdw)
+
 // API Get ALl User Admin
-router.get("/",authMdw.authMdw, authMdw.adminMdw, userAdminController.fetchAllUser)
+router.get("/", userAdminController.fetchAllUser)
 
 //API Get User
-router.get("/:id",authMdw.authMdw, authMdw.adminMdw, userAdminController.fetchUser);
-
-// API Register User
-router.post("/", userAdminController.registerUser);
+router.get("/:id", userAdminController.fetchUser);
 
 //API Update Information User
-router.put("/:id",authMdw.authMdw, authMdw.adminMdw, userAdminController.updateIn4User);
+router.put("/:id", userAdminController.updateIn4User);
 
 //API Delete User Admin
-router.delete("/:id",authMdw.authMdw, authMdw.adminMdw, userAdminController.deleteUser);
+router.delete("/:id", userAdminController.deleteUser);
 
 export default router;
