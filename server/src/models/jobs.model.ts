903f89f8cec4194ce01e64d768d1f4c4be9091f9
shanele147 <shane.le147@gmail.com>
2023-04-17 23:19:05 +0700

    test POST new job

--- server/src/models/jobs.model.ts
+++ server/src/models/jobs.model.ts
@@ -17,7 +17,6 @@ export interface IJob {
   const jobSchema: Schema = new mongoose.Schema({
     title: { type: String, required: true },
     location: { type: String, required: true },
-    content: { type: String, required: true },
     type: { type: String, required: true },
     tags: { type: Array, required: true },
     created_at: { type: Date, default: Date.now() },
