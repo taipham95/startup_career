b1fd1a1458c436f75b98645ee7fed5209b7f5b54
tranminhwang <j2team.tranminhquang@gmail.com>
2023-04-17 20:55:16 +0700

    update content field

--- client/src/pages/AdminPage/index.jsx
+++ client/src/pages/AdminPage/index.jsx
@@ -3,6 +3,7 @@ import Sidebar from "./Sidebar";
 import Overview from "./Overview/Overview";
 import CandidatesPage from "./CandidatesPage";
 
+import UpdateJobPage from "./UpdateJobPage/UpdateJobPage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import JobCreatorPage from './JobCreatorPage/JobCreatorPage';
 
@@ -17,6 +18,7 @@ const AdminPage = () => {
             <Route exact path="/" element={<Overview />} />
             <Route exact path="/candidates" element={<CandidatesPage />} />
             <Route exact path="/create-job" element={<JobCreatorPage />} />
+            <Route exact path="/update-job/:id" element={<UpdateJobPage />} />
           </Routes>
         </div>
       </main>
