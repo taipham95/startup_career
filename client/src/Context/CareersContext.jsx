e39a02cf26c321532131ed83d0959d75c78aa6e8
DESKTOP-A02GDQH\DELL <phuclethinh70@gmail.com>
2023-04-26 16:18:20 +0700

    c

--- client/src/Context/CareersContext.jsx
+++ client/src/Context/CareersContext.jsx
@@ -12,7 +12,7 @@ export const AppProvider = ({ children }) => {
 
   useEffect(() => {
     const handleFetchJobs = async () => {
-      const response = await dataServices.getData(`${BASE_URL}${JOBS_ENDPOINT}`);
+      const response = await dataService.getData(`${BASE_URL}${JOBS_ENDPOINT}`);
       setJobsData(response.data);
       // console.log("day la respon ne : ",response.data.data);
       console.log("day la respon ne : ",response.data);
